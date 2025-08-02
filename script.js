f6361015b5e523dcba1099f7563e8546c1cc055a
Willie Loftie-Eaton <24717274@sun.ac.za>
2025-08-02 23:30:52 +0200

    More changes

--- script.js
+++
@@ -1,9 +0,0 @@
-// I don't know any JavaScript. ChatGPT wrote this.
-const toggleBtns = document.querySelectorAll("#toggle-btn");
-
-toggleBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    localStorage.setItem("dark-mode", document.body.classList.contains("dark-mode"));
-  });
-});
